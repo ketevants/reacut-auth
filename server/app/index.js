a33b1b44fd04a9e92f813d26f5f7bdbc4b2df339
Connie Lee <connie.lee@fullstackacademy.com>
2017-05-22 17:47:14 -0500

    fix: error handling

--- server/app/index.js
+++ server/app/index.js
@@ -3,6 +3,8 @@
 var app = require('express')();
 var path = require('path');
 
+var HttpError = require('../utils/HttpError');
+
 // "Enhancing" middleware (does not send response, server-side effects only)
 
 app.use(require('./logging.middleware'));
@@ -24,6 +26,24 @@ validFrontendRoutes.forEach(function (stateRoute) {
 
 app.use(require('./statics.middleware'));
 
-app.use(require('./error.middleware'));
+// Error handling middleware
+
+app.use(HttpError(404).middleware());
+
+app.use(function (err, req, res, next) {
+  err.status = err.status || 500;
+  console.error(err.stack);
+  var html = [
+    '<html><body>',
+    '<p>ERROR: ', err.status, ' - ', err.message, '</p>',
+    '<p>VERB: ', req.method, '</p>',
+    '<p>URL: ', req.originalUrl, '</p>',
+    '<p>QUERY ', JSON.stringify(req.query), '</p>',
+    '<p>BODY: ', JSON.stringify(req.body), '</p>',
+    '<pre>', err.stack, '</pre>',
+    '</body></html>'
+  ].join('');
+  res.status(err.status).send(html);
+});
 
 module.exports = app;
